3f893c59de831d8deeaae44069ac55bd977f5d4a
Jakob Hagl <m1a2.jakob@gmail.com>
2020-10-01 15:54:13 +0200

    Add aws_api container

---
+++ src/aws_api/api/config/database.php
@@ -0,0 +1,26 @@
+<?php
+class Database{
+  
+    // specify your own database credentials
+    private $host = "aws_database";
+    private $db_name = "storagehouse";
+    private $username = "aws";
+    private $password = "aws_pw";
+    public $conn;
+  
+    // get the database connection
+    public function getConnection(){
+  
+        $this->conn = null;
+  
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        }catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+  
+        return $this->conn;
+    }
+}
+?>
